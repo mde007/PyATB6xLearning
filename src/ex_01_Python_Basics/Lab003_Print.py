0654303e66c504e1ea73c6a3852f3ac9716d97bb
mde007 <moumita.de01@gmail.com>
2025-10-10 21:33:11 +0530

    Commit1

---
+++ src/ex_01_Python_Basics/Lab003_Print.py
@@ -0,0 +1,13 @@
+print("Hello World!")
+# print(self, *args, sep=' ', end='\n', file=None):
+
+# self - please ignore  (oops)
+# *args - unlimited number of comma seperated arguments.
+print("PramodDutta", 123, "Amit", "John")
+print("PramodDutta", 123, "Amit", "John", "Vinod", "Roznin", "Ankesh", True, 3.145)
+print("PramodDutta", 123, "Amit", "John", "Vinod", "Roznin", "Ankesh", True, 3.145, sep="-")
+print("PramodDutta", 123, "Amit", "John", "Vinod", "Roznin", "Ankesh", True, 3.145, sep="*", end="\n")
+
+# IndentationError: unexpected indent
+print("PramodDutta", 123, "Amit", "John", "Vinod", "Roznin", "Ankesh", True, 3.145, sep="*", end="!!!")
+# Print() - Case sensitive
