0d31c09a375a6060cec991a931e5cba796631cf1
mde007 <moumita.de01@gmail.com>
2025-10-10 21:37:41 +0530

    Commit1

---
+++ src/ex_02_Keywords_Identifier_Variables/Lab005_Variables_Part1.py
@@ -0,0 +1,11 @@
+age = 65
+age  = "Pramod"
+# Dynamic Typed Language. - It can understand you are doing, want.
+pi = 3.14
+name = "Pramod"
+name = "Pramod"
+name = "Pramod"
+name = "Pramod"
+name = "Pramod"
+name = "Dutta"
+print(name)
---
+++ src/ex_02_Keywords_Identifier_Variables/Lab006_Identifier.py
@@ -0,0 +1,13 @@
+age = 65
+_age = 66
+print(age)
+print(_age)
+
+_ = 12
+print(_)
+_ = _+1+_
+print(_)
+
+#123abc = 90
+abc123 = "abc123"
+print(abc123)
