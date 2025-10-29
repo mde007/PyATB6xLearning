af8e4dd91bceb76ab186feba049ef7ea0fcee30f
mde007 <moumita.de01@gmail.com>
2025-10-29 13:05:36 +0530

    Oct29

--- src/ex_03_Literals_Variables/Lab030_Task2.py
+++ src/ex_03_Literals_Variables/Lab030_Task2.py
@@ -10,7 +10,7 @@ num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 
 division = num1 / num2 #True division, Result is always float showing fractional part as well
-quotient = num1 // num2 #Floor division, doesnot return fractional part, only shows quotient
+quotient = num1 // num2 #Floor/Integer division, doesnot return fractional part, only shows quotient
 remainder = num1 % num2
 
 print(division)
