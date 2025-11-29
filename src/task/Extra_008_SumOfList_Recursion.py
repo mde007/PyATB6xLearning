5abbd7f8a48f4fae19a06f67cfc84b296607710a
mde007 <moumita.de01@gmail.com>
2025-11-29 22:59:41 +0530

    Nov 29

--- src/task/Extra_008_SumOfList_Recursion.py
+++ src/task/Extra_008_SumOfList_Recursion.py
@@ -1,12 +1,23 @@
 # Sum of List: Calculate the sum of all elements in a list using recursion
-"""
-num1 = int(input("Enter first integer: \n"))
-num2 = int(input("Enter second integer: \n"))
 
-if num1 > num2:
-    print("First num is greater")
-elif num1 == num2:
-    print("Both numbers are equal")
-else:
-    print("Second num is greater")
+""" Normal way using for loop
+def sum_func(list1):
+    sum_list = 0
+    for element in list1:
+        sum_list += element
+    print(sum_list)
+
+my_list = [1, 2, 3, 4, 5.6, 6]
+sum_func(my_list)
 """
+
+def recursive_list_sum(elements, x):
+    if x == 0: #empty list scenario
+        return 0
+    else:
+        return elements[x-1] + recursive_list_sum(elements, x-1)
+
+my_list = [1, 2, 3, 4, 5.6]
+n = len(my_list)
+total_sum = recursive_list_sum(my_list, n)
+print(f"The sum of the list elements is: {total_sum}")
--- src/task/Extra_009_Largest_Smallest_List.py
+++ src/task/Extra_009_Largest_Smallest_List.py
@@ -1,12 +1,6 @@
 # Write a Python program to find the largest number and smallest number in a list
-"""
-num1 = int(input("Enter first integer: \n"))
-num2 = int(input("Enter second integer: \n"))
 
-if num1 > num2:
-    print("First num is greater")
-elif num1 == num2:
-    print("Both numbers are equal")
-else:
-    print("Second num is greater")
-"""
+my_list = [1, 12, 3, 4, 5.6, 6, 5.06, 0.01]
+
+print("Largest number in the given list: ", max(my_list))
+print("Smallest number in the given list: ", min(my_list))
