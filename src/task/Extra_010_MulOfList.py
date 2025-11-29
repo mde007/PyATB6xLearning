2904b14a17b9af3f8fd6eb78f403fbd1fb45862b
mde007 <moumita.de01@gmail.com>
2025-11-30 00:43:23 +0530

    Nov 29

--- src/task/Extra_010_MulOfList.py
+++ src/task/Extra_010_MulOfList.py
@@ -1,12 +1,14 @@
 # Write a Python program to multiply all numbers in a list
-"""
-num1 = int(input("Enter first integer: \n"))
-num2 = int(input("Enter second integer: \n"))
 
-if num1 > num2:
-    print("First num is greater")
-elif num1 == num2:
-    print("Both numbers are equal")
-else:
-    print("Second num is greater")
-"""
+def mul_func(list1):
+    product = 1
+    for element in list1:
+        if element == 0:
+            return 0
+        else:
+            product *= element
+    return product
+
+
+my_list = [1, 2, 3, 4, 5, 10]
+print("Multiplication product of the list elements: ", mul_func(my_list))
--- src/task/Extra_011_CountNumberOfStrings_List.py
+++ src/task/Extra_011_CountNumberOfStrings_List.py
@@ -1,13 +1,12 @@
 # Write a Python program to count the number of strings in a list where
 # the string length is 2 or more and the first and last characters are the same
-"""
-num1 = int(input("Enter first integer: \n"))
-num2 = int(input("Enter second integer: \n"))
 
-if num1 > num2:
-    print("First num is greater")
-elif num1 == num2:
-    print("Both numbers are equal")
-else:
-    print("Second num is greater")
-"""
+def count_strings(list1):
+    c=0
+    for elements in my_list:
+        if len(elements) > 1 and elements[0] == elements[-1]:
+            c += 1
+    return c
+
+my_list = ["aa", "bcb", "de", "fgh", "cccc", "setts"]
+print("Count is: ", count_strings(my_list))
