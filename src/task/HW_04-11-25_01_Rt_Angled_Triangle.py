9c6a21d58327996b55d723ea42525643e47b1b03
mde007 <moumita.de01@gmail.com>
2025-12-07 21:21:24 +0530

    Dec7

--- src/task/HW_04-11-25_01_Rt_Angled_Triangle.py
+++ src/task/HW_04-11-25_01_Rt_Angled_Triangle.py
@@ -6,18 +6,21 @@ Write a program to print a right angled triangle with *
 
 ***
 """
+
+
 def lt_align_triangle(rows):
-    for i in range(1, rows+1):
-        for j in range(1, i+1):
-            print("*", end='') # Print asterisks
+    for i in range(1, rows + 1):
+        for j in range(i):
+            print("*", end='')  # Print asterisks
         print("\n")
+
 
 def rt_align_triangle(rows):
     for i in range(1, rows + 1):
         print(" " * (rows - i), end="")  # Print leading spaces
-        print("*" * i) # Print asterisks
+        print("*" * i)  # Print asterisks
 
-n= int(input("Enter number of rows: "))
-rt_align_triangle(n)
+
+n = int(input("Enter number of rows: "))
 lt_align_triangle(n)
-
+rt_align_triangle(n)
---
+++ src/task/HW_13-11-25_01_PersonClass.py
@@ -0,0 +1,41 @@
+# Create a Person class where we will have five attributes and five behaviors.
+# Make sure that each type of function is used, and I want you to also create the print function,
+# which will print all the instance variable values.
+
+class Person:
+    a = 10
+    b = 20
+    c = 30
+    d = 40
+    e = 50
+
+    def __init__(self):
+        print("DC is called")
+
+    def func1(self):
+        print("Func1")
+
+    def func2(self):
+        print("Func2")
+
+    def func3(self):
+        print("Func3")
+
+    def func4(self):
+        print("Func4")
+
+    def func5(self):
+        print("Func5")
+
+
+md = Person()
+print(md.a)
+print(md.b)
+print(md.c)
+print(md.d)
+print(md.e)
+md.func1()
+md.func2()
+md.func3()
+md.func4()
+md.func5()
